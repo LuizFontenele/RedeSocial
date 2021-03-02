5ab73001aa7cfee1f767cd66b359ff37f22cebdd
LuizFontenele <luizfelper@gmail.com>
2021-03-02 14:56:26 -0300

    Codigo Inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+import Header from './components/header';
+
+function Home() {
+    return (
+        <div>
+            <Header></Header>
+            <main className="main">
+                <div className="navbar">
+                    navbar
+                </div>
+                <div className="feed">
+                    feed
+                </div>
+            </main>
+
+        </div>
+    );
+}
+
+export default Home;
